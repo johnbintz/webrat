0856d8d405376f743f21bb3920830923f1ba060f
John Bintz <john@coswellproductions.com>
2010-06-01 14:10:39 -0400

    potential fix for invalid query string construction

--- lib/webrat/core/elements/form.rb
+++ lib/webrat/core/elements/form.rb
@@ -110,7 +110,7 @@ module Webrat
       when :rack, :sinatra
         Rack::Utils.parse_nested_query(query_string)
       else
-        query_string.split('&').map {|query| { query.split('=').first => query.split('=').last }}
+        Hash[query_string.split('&').map {|query| [ query.split('=').first, query.split('=').last ]}]
       end
     end
 
